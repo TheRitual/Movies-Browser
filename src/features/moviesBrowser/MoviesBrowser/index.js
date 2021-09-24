5135f52aa326741339f2f19aeda4d62a6e4016f5
Ritual <dev@theritual.eu>
2021-09-24 15:38:11 +0200

    Added Pages components to routes

--- src/features/moviesBrowser/MoviesBrowser/index.js
+++ src/features/moviesBrowser/MoviesBrowser/index.js
@@ -1,32 +1,36 @@
 import { Switch, Route, HashRouter, Redirect } from "react-router-dom";
 import { toActor, toActorsList, toMovie, toMoviesList } from "../../../core/config/routes";
+import MovieDetailPage from "./MovieDetailPage";
+import ActorDetailPage from "./ActorDetailPage";
+import MoviesListPage from "./MoviesListPage";
+import ActorsListPage from "./ActorsListPage";
 import { Main } from "./styled";
 
 const MoviesBrowser = () => {
-    return (
-        <HashRouter>
-            <nav>Navigation</nav>
-            <Main>
-                <Switch>
-                    <Route path={toMoviesList()}>
-                        <div>features/moviesBrowser/MoviesListPage</div>
-                    </Route>
-                    <Route path={toMovie()}>
-                        <div>features/movieBrowser/MovieDetailPage</div>
-                    </Route>
-                    <Route path={toActorsList()}>
-                        <div>features/moviesBrowser/ActorsListPage</div>
-                    </Route>
-                    <Route path={toActor()}>
-                        <div>features/movieBrowser/ActorDetailPage</div>
-                    </Route>
-                    <Route>
-                        <Redirect to={toMoviesList()} />
-                    </Route>
-                </Switch>
-            </Main>
-        </HashRouter>
-    );
+  return (
+    <HashRouter>
+      <nav>Navigation</nav>
+      <Main>
+        <Switch>
+          <Route path={toMoviesList()}>
+            <MoviesListPage />
+          </Route>
+          <Route path={toMovie()}>
+            <MovieDetailPage />
+          </Route>
+          <Route path={toActorsList()}>
+            <ActorsListPage />
+          </Route>
+          <Route path={toActor()}>
+            <ActorDetailPage />
+          </Route>
+          <Route>
+            <Redirect to={toMoviesList()} />
+          </Route>
+        </Switch>
+      </Main>
+    </HashRouter>
+  );
 }
 
 export default MoviesBrowser;
