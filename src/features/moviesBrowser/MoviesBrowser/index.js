e0e06eda7b4ca598e83d51a293d38d4cbad83d33
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-24 11:39:03 +0200

    Tile imported

--- src/features/moviesBrowser/MoviesBrowser/index.js
+++ src/features/moviesBrowser/MoviesBrowser/index.js
@@ -1,4 +1,6 @@
 import { Main } from "./styled";
+import { Tile } from "../../Tile";
+import { Image } from "../../images/poster.png";
 
 const MoviesBrowser = () => {
     return (
@@ -7,6 +9,25 @@ const MoviesBrowser = () => {
         <Main>
             Main Page Content
         </Main>
+        <Tile>
+        <Image src={poster} alt="" />
+        <Content>
+          <Title>Mulan</Title>
+          <Subtitle>
+          2020
+          </Subtitle>
+          <Tags>
+            <Tag>Action</Tag>
+            <Tag>Adventure</Tag>
+            <Tag>Drama</Tag>
+          </Tags>
+          <Description>
+            A young Chinese maiden disguises herself as a male warrior in order to save her father.
+            Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father.
+          </Description>
+
+        </Content>
+        </Tile>
         </>
     );
 }
