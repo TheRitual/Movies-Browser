e0070dda2c9bb19afc47044817d50e00fb97ce38
Ritual <dev@theritual.eu>
2021-09-24 17:03:12 +0200

    deleted generic directory

--- src/generic/components/Navigation/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledNavigation = styled.nav`
-    width: 100%;
-    height: 94px;
-    background-color: ${({ theme }) => theme.colors.navBackground};
-    color: ${({ theme }) => theme.colors.navText};
-    padding: 23px;
-`;
