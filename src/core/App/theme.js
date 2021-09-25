4d9534ce9053dc1c6b714949901be8ba95c25bdf
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-25 13:44:19 +0200

    added mystic and woodsmoke color

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,11 +1,14 @@
 const pallette = {
     white: "#FFFFFF",
-    black: "#18181B",
+    black: "#00000",
+    woodsmoke: "#18181B",
+    mystic: "#E4E6F0",
 }
 
 export const theme = {
     colors: {
         bodyBackground: pallette.white,
+        navigationBar: pallette.woodsmoke,
     },
     breakpoints: {
         tabletPortrait: "767px",
