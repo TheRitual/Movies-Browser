0b5e62e2b9e33b476231805c4de26395d2a9879b
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-25 20:55:36 +0200

    added parameter to NavLink

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,33 +1,40 @@
 import React from "react";
-import { 
+import {
     StyledNavigation,
     NavigationList,
     StyledTitle,
     StyledNavLink,
     StyledNavWrapper,
     StyledInput
- } from "./styled";
+} from "./styled";
+import { toActorsList, toMoviesList } from "../../core/config/routes";
 
 
 const Navigation = () => {
-return(
-    <StyledNavigation>
-        <StyledNavWrapper>
-            <>StyledCameraIcon</>
-            {/*tu miejsce na ikonkę SVG kamery*/}
-                        <StyledTitle>Movies Browser</StyledTitle>
-            <NavigationList>
-                <StyledNavLink>Movies</StyledNavLink>
-                <StyledNavLink>People</StyledNavLink>
+    return (
+        <StyledNavigation>
+            <StyledNavWrapper>
+                <>StyledCameraIcon</>
+                {/*tu miejsce na ikonkę SVG kamery*/}
+                <StyledTitle>Movies Browser</StyledTitle>
+                <NavigationList>
+                    <StyledNavLink 
+                    to={toMoviesList()}
+                    >
+                        Movies</StyledNavLink>
+                    <StyledNavLink 
+                    to={toActorsList()}
+                    >
+                        People </StyledNavLink>
                 </NavigationList>
                 <StyledInput>
                     <>InputIcon</>
                     {/*tu będzie ikonka search SVG <>InputIcon */}
                     Search
                 </StyledInput>
-        </StyledNavWrapper>
-    </StyledNavigation>
-)
+            </StyledNavWrapper>
+        </StyledNavigation>
+    )
 }
 
 export default Navigation;
