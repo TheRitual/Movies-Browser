e9e4c493a467e9db309e41faacfa7cfbd0dcb81d
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-25 20:38:30 +0200

    typos corrected

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-export const StyledNavigation = styled.navigation`
+export const StyledNavigation = styled.nav`
 width: 1920px;
 height: 94px;
-background-color: ${({ theme }) => theme.color.navigationBar};
+background-color: ${({ theme }) => theme.colors.navigationBar};
 left: 0%;
 right: 0%;
 top: 0%;
@@ -24,11 +24,11 @@ margin: 0;
 export const StyledCameraIcon = styled.img`
 width: 22px;
 height: 23px;
-border: 2.5px solid ${({ theme }) => theme.color.white};
+border: 2.5px solid ${({ theme }) => theme.colors.white};
 `;
 
 export const StyledTitle = styled.h1`
-color: ${({ theme }) => theme.color.white};
+color: ${({ theme }) => theme.colors.white};
 position: absolute;
 width: 168px;
 height: 40px;
@@ -45,14 +45,14 @@ text-transform: capitalize;
 `;
 
 export const StyledInput = styled.input`
-background-color: ${({ theme }) => theme.color.white};
+background-color: ${({ theme }) => theme.colors.white};
 border-radius: 33px;
 position: absolute;
 left: 0%;
 right: 0%;
 top: 0%;
 bottom: 0%;
-border: 1px solid ${({ theme }) => theme.color.mystic};
+border: 1px solid ${({ theme }) => theme.colors.mystic};
 `;
 
 export const InputIcon = styled.img`
@@ -70,10 +70,10 @@ flex-wrap: wrap;
 `;
 
 
-export const StyledNavLink = styled(NavLink.attrs(() => ({
+export const StyledNavLink = styled(NavLink).attrs(() => ({
     activeClassName: "active",
 }))`
-    color: ${({ theme }) => theme.color.white};
+    color: ${({ theme }) => theme.colors.white};
     text-decoration: none;
     font-family: Poppins;
     font-style: normal;
@@ -81,4 +81,4 @@ export const StyledNavLink = styled(NavLink.attrs(() => ({
     font-size: 14px;
     line-height: 21px;
     text-transform: uppercase;
-    `; 
+    `;
