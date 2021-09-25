f5c5bdf4c4f3b0ae22ce7d204a8b42e885f11cd4
Ritual <dev@theritual.eu>
2021-09-25 18:05:35 +0200

    closed bracket

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -2,77 +2,75 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const StyledNavigation = styled.navigation`
-width: 1920px;
-height: 94px;
-background-color: ${({ theme }) => theme.color.navigationBar};
-left: 0%;
-right: 0%;
-top: 0%;
-display: flex;
-justify-content: center;
-padding: 10px;
+    width: 1920px;
+    height: 94px;
+    background-color: ${({ theme }) => theme.color.navigationBar};
+    left: 0%;
+    right: 0%;
+    top: 0%;
+    display: flex;
+    justify-content: center;
+    padding: 10px;
 `;
 
 export const NavigationList = styled.ul`
-list-style: none;
-display: flex;
-justify-content: center;
-width: max-content;
-margin: 0;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    width: max-content;
+    margin: 0;
 `;
 
 export const StyledCameraIcon = styled.img`
-width: 22px;
-height: 23px;
-border: 2.5px solid ${({ theme }) => theme.color.white};
+    width: 22px;
+    height: 23px;
+    border: 2.5px solid ${({ theme }) => theme.color.white};
 `;
 
 export const StyledTitle = styled.h1`
-color: ${({ theme }) => theme.color.white};
-position: absolute;
-width: 168px;
-height: 40px;
-left: 52px;
-top: calc(50% - 40px/2);
-margin: 0;
-font-family: Poppins;
-font-style: normal;
-font-weight: 500;
-font-size: 24px;
-line-height: 40px;
-letter-spacing: -1.5px;
-text-transform: capitalize;
+    color: ${({ theme }) => theme.color.white};
+    position: absolute;
+    width: 168px;
+    height: 40px;
+    left: 52px;
+    top: calc(50% - 40px/2);
+    margin: 0;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 40px;
+    letter-spacing: -1.5px;
+    text-transform: capitalize;
 `;
 
 export const StyledInput = styled.input`
-background-color: ${({ theme }) => theme.color.white};
-border-radius: 33px;
-position: absolute;
-left: 0%;
-right: 0%;
-top: 0%;
-bottom: 0%;
-border: 1px solid ${({ theme }) => theme.color.mystic};
+    background-color: ${({ theme }) => theme.color.white};
+    border-radius: 33px;
+    position: absolute;
+    left: 0%;
+    right: 0%;
+    top: 0%;
+    bottom: 0%;
+    border: 1px solid ${({ theme }) => theme.color.mystic};
 `;
 
 export const InputIcon = styled.img`
-width: 20px;
-height: 20px;
+    width: 20px;
+    height: 20px;
 
 `;
 
 export const StyledNavWrapper = styled.div`
-display: flex;
-align-items: center;
-width: 100%;
-justify-content: space-between;
-flex-wrap: wrap;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    flex-wrap: wrap;
 `;
 
 
-export const StyledNavLink = styled(NavLink.attrs(() => ({
-    activeClassName: "active",
-}))`
+export const StyledNavLink = styled(NavLink).attrs(() => ({ activeClassName: "active", }))`
     color: ${({ theme }) => theme.color.white};
     text-decoration: none;
     font-family: Poppins;
@@ -81,4 +79,4 @@ export const StyledNavLink = styled(NavLink.attrs(() => ({
     font-size: 14px;
     line-height: 21px;
     text-transform: uppercase;
-    `; 
+`;
