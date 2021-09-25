0f5a8b4bafca6590b082093e8fd1b246761b372f
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-25 17:07:28 +0200

    import components from styled

--- src/features/moviesBrowser/MoviesBrowser/MovieDetailPage/index.js
+++ src/features/moviesBrowser/MoviesBrowser/MovieDetailPage/index.js
@@ -1,7 +1,37 @@
+import {
+    Tile,
+    Title,
+    Subtitle,
+    Content,
+    Tags,
+    Tag,
+    Description,
+    
+} from "./styled";
+
 const MovieDetailPage = () => {
     return (
         <>
             MovieDetailPage
+            <Tile>
+        {/*tu miejsce na SVG <Image src={poster} alt="" /> */}
+        <Content>
+          <Title>Mulan</Title>
+          <Subtitle>
+          2020
+          </Subtitle>
+          <Tags>
+            <Tag>Action</Tag>
+            <Tag>Adventure</Tag>
+            <Tag>Drama</Tag>
+          </Tags>
+          <Description>
+            A young Chinese maiden disguises herself as a male warrior in order to save her father.
+            Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father.
+          </Description>
+
+        </Content>
+      </Tile>
         </>
     );
 }
