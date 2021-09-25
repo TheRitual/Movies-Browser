1fa27055587ce45cc97b6073a19888fb44a4e093
Ritual <dev@theritual.eu>
2021-09-25 21:17:00 +0200

    Changed MovieBrowser component rotes and added NotFound component

--- src/features/moviesBrowser/MoviesBrowser/index.js
+++ src/features/moviesBrowser/MoviesBrowser/index.js
@@ -6,6 +6,7 @@ import MoviesListPage from "../MoviesListPage";
 import ActorsListPage from "../ActorsListPage";
 import Navigation from "../../../common/Navigation";
 import { Main } from "./styled";
+import NotFound from "../../../common/NotFound";
 
 const MoviesBrowser = () => {
   return (
@@ -13,21 +14,14 @@ const MoviesBrowser = () => {
       <Navigation />
       <Main>
         <Switch>
-          <Route path={toMoviesList()}>
-            <MoviesListPage />
-          </Route>
-          <Route path={toMovie()}>
-            <MovieDetailPage />
-          </Route>
-          <Route path={toActorsList()}>
-            <ActorsListPage />
-          </Route>
-          <Route path={toActor()}>
-            <ActorDetailPage />
-          </Route>
-          <Route>
+          <Route path={toMoviesList()} component={MoviesListPage} />  
+          <Route path={toMovie()} component={MovieDetailPage} />          
+          <Route path={toActorsList()} component={ActorsListPage} />
+          <Route path={toActor()} component={ActorDetailPage} />
+          <Route exact path="/">
             <Redirect to={toMoviesList()} />
           </Route>
+          <Route component={NotFound} />
         </Switch>
       </Main>
     </HashRouter>
