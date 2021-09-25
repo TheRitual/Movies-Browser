aa51d45bcae504d934c4b4b68dd9e01bfed19fa5
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-25 20:21:22 +0200

    navigation added

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,9 +1,33 @@
+import React from "react";
+import { 
+    StyledNavigation,
+    NavigationList,
+    StyledTitle,
+    StyledNavLink,
+    StyledNavWrapper,
+    StyledInput
+ } from "./styled";
+
+
 const Navigation = () => {
-    return (
-        <>
-            Navigation
-        </>
-    );
+return(
+    <StyledNavigation>
+        <StyledNavWrapper>
+            <>StyledCameraIcon</>
+            {/*tu miejsce na ikonkę SVG kamery*/}
+                        <StyledTitle>Movies Browser</StyledTitle>
+            <NavigationList>
+                <StyledNavLink>Movies</StyledNavLink>
+                <StyledNavLink>People</StyledNavLink>
+                </NavigationList>
+                <StyledInput>
+                    <>InputIcon</>
+                    {/*tu będzie ikonka search SVG <>InputIcon */}
+                    Search
+                </StyledInput>
+        </StyledNavWrapper>
+    </StyledNavigation>
+)
 }
 
 export default Navigation;
