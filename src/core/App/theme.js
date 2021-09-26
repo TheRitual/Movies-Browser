f3bb91c39a4494faba0d9ecda9af1591166467bc
Ewelina Niewęgłowska <nieweglowska.ewelina@gmail.com>
2021-09-26 21:05:27 +0200

    LoadingPage component created

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,6 +1,7 @@
 const pallette = {
     white: "#FFFFFF",
     black: "#000000",
+    mercury: "#E5E5E5",
 }
 
 export const theme = {
@@ -8,8 +9,10 @@ export const theme = {
         bodyBackground: pallette.white,
         navBackground: pallette.black,
         navText: pallette.white,
+        subpageBackground: pallette.mercury,
     },
     breakpoints: {
+        small: 576,
         tabletPortrait: "767px",
     }
 }
