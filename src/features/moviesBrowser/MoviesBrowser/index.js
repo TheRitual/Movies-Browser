be75a0567386104b402c8e26c5d91da3b1d5ad6c
Ritual <dev@theritual.eu>
2021-09-27 20:06:02 +0200

    removed fetching data for a while

--- src/features/moviesBrowser/MoviesBrowser/index.js
+++ src/features/moviesBrowser/MoviesBrowser/index.js
@@ -1,25 +1,18 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
 import { HashRouter, Link } from "react-router-dom";
 import { toPeopleList, toMoviesList } from "../../../core/config/routes";
-import { fetchMoviesListData, fetchPeopleListData, selectIsLoading } from "../moviesBrowserSlice";
+import { selectIsLoading } from "../moviesBrowserSlice";
 import PageSwitch from "./PageSwitch";
 import LoadingPage from "../../../common/LoadingPage";
 import { Main } from "./styled";
 
 
 const MoviesBrowser = () => {
-  const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
-  useEffect(() => {
-    dispatch(fetchMoviesListData());
-    // eslint-disable-next-line
-  }, []);
   return (
     <HashRouter>
-      <Link to={toMoviesList()} onClick={() => dispatch(fetchMoviesListData())}> Movies </Link>
-      <Link to={toPeopleList()} onClick={() => dispatch(fetchPeopleListData())}> People </Link>
+      <Link to={toMoviesList()}> Movies </Link>
+      <Link to={toPeopleList()}> People </Link>
       <Main>
         {isLoading ?
           <LoadingPage />
