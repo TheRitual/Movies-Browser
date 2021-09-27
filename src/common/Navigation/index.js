229f1d05f6efac68df681889a2030fd2abb7446e
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-27 10:19:50 +0200

    styled updated

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,9 +1,34 @@
 const Navigation = () => {
     return (
-        <>
-            Navigation
-        </>
-    );
+        <StyledNavigation>
+            <StyledNavWrapper>
+                <>StyledCameraIcon</>
+                {/*tu miejsce na ikonkę SVG kamery*/}
+                <StyledTitle>Movies Browser</StyledTitle>
+                <NavigationList>
+            
+                    <StyledNavLink 
+                    to={toMoviesList()}
+                    >
+                        Movies
+                        </StyledNavLink>
+                     
+              
+                    <StyledNavLink 
+                    to={toActorsList()}
+                    >
+                        People 
+                        </StyledNavLink>
+                     
+                </NavigationList>
+                <StyledInput />
+                    <>InputIcon</>
+                    {/*tu będzie ikonka search SVG <>InputIcon */}
+                    Search
+                
+            </StyledNavWrapper>
+        </StyledNavigation>
+    )
 }
 
 export default Navigation;
