0136c6adada6b9520d3793fb9f578127b7d3b1ee
Ritual <dev@theritual.eu>
2021-09-29 13:09:55 +0200

    changed LoadingPage to Loading

--- src/features/MoviesBrowser/PersonDetailPage/PersonDetails/index.js
+++ src/features/MoviesBrowser/PersonDetailPage/PersonDetails/index.js
@@ -2,14 +2,14 @@ import { useSelector } from "react-redux";
 import { selectIsLoading } from "../../moviesBrowserSlice";
 import PersonDetailsTile from "../PersonDetailsTile";
 import MoviesList from "../MoviesList";
-import LoadingPage from "../../../../common/LoadingPage";
+import Loading from "../../../../common/Loading";
 
 const PersonDetails = () => {
     const isLoading = useSelector(selectIsLoading);
     return isLoading ?
         <>
             <h1>Loading Person Details...</h1>
-            <LoadingPage />
+            <Loading />
         </>
         :
         <>
