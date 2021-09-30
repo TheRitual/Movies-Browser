9dbbfea7cc02f719516d16a39f3304963b6f4a71
Ritual <dev@theritual.eu>
2021-09-30 16:04:11 +0200

    added switching pages and fetching new data

--- src/features/MoviesBrowser/ListPage/Paginator/index.js
+++ src/features/MoviesBrowser/ListPage/Paginator/index.js
@@ -1,17 +1,31 @@
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { selectPage, selectTotalPages } from "../../moviesBrowserSlice";
+import { fetchMoviesListData, fetchPeopleListData, fetchSearchData, selectPage, selectSearchQuery, selectTotalPages, selectType, setPage } from "../../moviesBrowserSlice";
 import { LeftArrow, PaginatorButton, PaginatorPage, PaginatorText, PaginatorWrapper, RightArrow } from "./styled";
 
 const Paginator = () => {
+    const dispatch = useDispatch();
     const page = useSelector(selectPage);
     const totalPages = useSelector(selectTotalPages);
+    const query = useSelector(selectSearchQuery);
+    const type = useSelector(selectType);
+
+    const onSetPage = (page) => {
+        dispatch(setPage(page));
+        if (query) {
+            dispatch(fetchSearchData());
+        } else {
+            type === "person" ? dispatch(fetchPeopleListData()) : dispatch(fetchMoviesListData());
+        }
+    }
+
     return (
         <PaginatorWrapper>
-            <PaginatorButton disabled>
+            <PaginatorButton disabled={page === "1" || page === 1} onClick={() => onSetPage(1)}>
                 <LeftArrow /> First
             </PaginatorButton>
 
-            <PaginatorButton disabled>
+            <PaginatorButton disabled={page === "1" || page === 1} onClick={() => onSetPage(page - 1)}>
                 <LeftArrow /> Previous
             </PaginatorButton>
 
@@ -19,11 +33,11 @@ const Paginator = () => {
                 Page <PaginatorPage>{page}</PaginatorPage> from <PaginatorPage>{totalPages}</PaginatorPage>
             </PaginatorText>
 
-            <PaginatorButton>
+            <PaginatorButton disabled={page === totalPages} onClick={() => onSetPage(page + 1)}>
                 Next <RightArrow />
             </PaginatorButton>
 
-            <PaginatorButton>
+            <PaginatorButton disabled={page === totalPages} onClick={() => onSetPage(totalPages)}>
                 Last <RightArrow />
             </PaginatorButton>
         </PaginatorWrapper>
