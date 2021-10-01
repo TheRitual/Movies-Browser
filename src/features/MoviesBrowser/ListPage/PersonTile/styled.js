7854ece6a7eb01535ea78690394e99165a2a1739
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-01 12:10:16 +0200

    StyledPersonTile component added and styled

--- src/features/MoviesBrowser/ListPage/PersonTile/styled.js
+++ src/features/MoviesBrowser/ListPage/PersonTile/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 
 export const StyledPersonTile = styled.div`
@@ -10,27 +10,4 @@ export const StyledPersonTile = styled.div`
     width: 208px;
     height: 339px;
 
-    ${({ horizontal }) =>
-        horizontal &&
-        css`
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 40px;
-
-    position: static;
-    width: 1368px;
-    height: 644px;
-    left: 0px;
-    top: 0px;
-    `}
-
-      @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-        margin: 0px;
-        width: 100%;
-        display: grid;
-        grid-gap: 16px;
-        grid-template-columns: 1fr;
-        align-items: center;
-      }``
 `;
