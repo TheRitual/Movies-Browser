584397baa200da78ec4e9ad1c2d31b5ea93d6a03
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-01 14:49:58 +0200

    StyleDetailTile, Title, Content and Tags imported

--- src/features/MoviesBrowser/MovieDetailPage/MovieDetailsTile/index.js
+++ src/features/MoviesBrowser/MovieDetailPage/MovieDetailsTile/index.js
@@ -1,39 +1,46 @@
 import { useSelector } from "react-redux";
 import Vote from "../../../../common/Vote";
 import { selectDetailItem } from "../../moviesBrowserSlice";
+import { StyledDetailTile, Title, Content, Description, Tags } from "./styled";
 
 const MovieDetailsTile = () => {
     const movie = useSelector(selectDetailItem);
     return (
-        <div>
-            <h4>Movie Details Tile</h4>
-            
-            <p>Title: {movie.title}</p>
+        <StyledDetailTile>
+            <div><Content>
+                <Title />
+                <h4>Movie Details Tile</h4>
 
-            <Vote score={movie.vote_average} count={movie.vote_count} />
-            
-            <p>Poster image : https://image.tmdb.org/t/p/w400{movie.poster_path}</p>
-            
-            <p> Release data: {movie.release_date} </p>
-            
-            <p> Countries:&nbsp;
-                {movie.production_countries && movie.production_countries.map(country => (
-                    <span key={country.iso_3166_1}>
-                        {country.name} ({country.iso_3166_1}),
-                    </span>
-                ))}
-            </p>
+                <p>Title: {movie.title}</p>
 
-            <p> Genres:&nbsp;
-                {movie.genres && movie.genres.map(genre => (
-                    <span key={genre.id}>
-                        [{genre.name}] 
-                    </span>
-                ))}
-            </p>
+                <Vote score={movie.vote_average} count={movie.vote_count} />
 
-            <p>Overview: {movie.overview}</p>
-        </div>
+                <p>Poster image : https://image.tmdb.org/t/p/w400{movie.poster_path}</p>
+
+                <p> Release data: {movie.release_date} </p>
+
+                <p> Countries:&nbsp;
+                    {movie.production_countries && movie.production_countries.map(country => (
+                        <span key={country.iso_3166_1}>
+                            {country.name} ({country.iso_3166_1}),
+                        </span>
+                    ))}
+                </p>
+                <Tags />
+                <p> Genres:&nbsp;
+                    {movie.genres && movie.genres.map(genre => (
+                        <span key={genre.id}>
+                            [{genre.name}]
+                        </span>
+                    ))}
+                </p>
+                <Description />
+                <p>Overview: {movie.overview}</p>
+            </Content>
+
+            </div>
+
+        </StyledDetailTile>
     );
 }
 
