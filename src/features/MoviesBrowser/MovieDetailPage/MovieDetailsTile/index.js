9e8f4fe392811c36e2edfbb33e72f4e53d942a3e
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-02 14:08:50 +0200

    Tags and Tag updated

--- src/features/MoviesBrowser/MovieDetailPage/MovieDetailsTile/index.js
+++ src/features/MoviesBrowser/MovieDetailPage/MovieDetailsTile/index.js
@@ -25,28 +25,28 @@ const MovieDetailsTile = () => {
                 <p>Poster image : https://image.tmdb.org/t/p/w400{movie.poster_path}</p>
 
                 <p> Release data: {movie.release_date} </p>
-<Tags>
-<p> Countries:&nbsp;
-                    {movie.production_countries && movie.production_countries.map(country => (
-                        <span key={country.iso_3166_1}>
-                            {country.name} ({country.iso_3166_1}),
-                        </span>
-                    ))}
-                </p>
-</Tags>
-                
-<Tags>
-<p> Genres:&nbsp;
-                    {movie.genres && movie.genres.map(genre => (
-    <Tag>
-        <span key={genre.id}>
-                            [{genre.name}]
-                        </span>
-    </Tag>                    
-                    ))}
-                </p>
-</Tags>
-                
+                <Tags>
+                    <p> Countries:&nbsp;
+                        {movie.production_countries && movie.production_countries.map(country => (
+                            <span key={country.iso_3166_1}>
+                                {country.name} ({country.iso_3166_1}),
+                            </span>
+                        ))}
+                    </p>
+                </Tags>
+
+                <Tags>
+                    <p> Genres:&nbsp;
+                        {movie.genres && movie.genres.map(genre => (
+                            <Tag>
+                                <span key={genre.id}>
+                                    [{genre.name}]
+                                </span>
+                            </Tag>
+                        ))}
+                    </p>
+                </Tags>
+
                 <Description />
                 <p>Overview: {movie.overview}</p>
             </Content>
