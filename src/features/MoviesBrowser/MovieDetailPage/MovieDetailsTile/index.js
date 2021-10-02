14ccecfd0080aecb63ffc8530cdf2581bee58455
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-02 14:08:37 +0200

    Tags and Tag imported

--- src/features/MoviesBrowser/MovieDetailPage/MovieDetailsTile/index.js
+++ src/features/MoviesBrowser/MovieDetailPage/MovieDetailsTile/index.js
@@ -6,7 +6,8 @@ import {
     Title,
     Content,
     Description,
-
+    Tags,
+    Tag,
 } from "./styled";
 
 const MovieDetailsTile = () => {
@@ -24,22 +25,28 @@ const MovieDetailsTile = () => {
                 <p>Poster image : https://image.tmdb.org/t/p/w400{movie.poster_path}</p>
 
                 <p> Release data: {movie.release_date} </p>
-
-                <p> Countries:&nbsp;
+<Tags>
+<p> Countries:&nbsp;
                     {movie.production_countries && movie.production_countries.map(country => (
                         <span key={country.iso_3166_1}>
                             {country.name} ({country.iso_3166_1}),
                         </span>
                     ))}
                 </p>
-
-                <p> Genres:&nbsp;
+</Tags>
+                
+<Tags>
+<p> Genres:&nbsp;
                     {movie.genres && movie.genres.map(genre => (
-                        <span key={genre.id}>
+    <Tag>
+        <span key={genre.id}>
                             [{genre.name}]
                         </span>
+    </Tag>                    
                     ))}
                 </p>
+</Tags>
+                
                 <Description />
                 <p>Overview: {movie.overview}</p>
             </Content>
