8a804ced7a638e2bac7fb79a9c98dd9865931f24
Ritual <dev@theritual.eu>
2021-10-05 10:23:30 +0200

    repaired posters at person details

--- src/features/MoviesBrowser/PersonDetailPage/CreditsList/index.js
+++ src/features/MoviesBrowser/PersonDetailPage/CreditsList/index.js
@@ -16,7 +16,7 @@ const CreditsList = () => {
                     <h4>Cast</h4>
                     {cast.map(castItem => (
                         <div key={castItem.id}>
-                            {castItem.profile_path && <img src={`https://image.tmdb.org/t/p/w185/${castItem.poster_path}`} alt={castItem.name} />}
+                            {castItem.poster_path && <img src={`https://image.tmdb.org/t/p/w200/${castItem.poster_path}`} alt={castItem.name} />}
                             <p><Link to={toMovie({ id: castItem.id })}>{castItem.title}</Link></p>
                             <p>{castItem.character}</p>
                         </div>
@@ -28,7 +28,7 @@ const CreditsList = () => {
                     <h4>Crew</h4>
                     {crew.map(crewItem => (
                         <div key={crewItem.id}>
-                            {crewItem.profile_path && <img src={`https://image.tmdb.org/t/p/w185/${crewItem.poster_path}`} alt={crewItem.name} />}
+                            {crewItem.poster_path && <img src={`https://image.tmdb.org/t/p/w200/${crewItem.poster_path}`} alt={crewItem.name} />}
                             <p><Link to={toMovie({ id: crewItem.id })}>{crewItem.title}</Link></p>
                             <p>{crewItem.job}</p>
                         </div>
