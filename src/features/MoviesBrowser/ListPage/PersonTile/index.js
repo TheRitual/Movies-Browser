505ed1a64c07c017be47d1f5f8d6dd13268fe320
Ritual <dev@theritual.eu>
2021-10-05 11:22:14 +0200

    added parameters to PersonTile

--- src/features/MoviesBrowser/ListPage/PersonTile/index.js
+++ src/features/MoviesBrowser/ListPage/PersonTile/index.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import { toPerson } from "../../../../core/config/routes";
 import { StyledPersonTile } from "./styled";
 
-const PersonTile = ({ person }) => {
+const PersonTile = ({ person, showJob, showCharacter }) => {
     return (
         <StyledPersonTile>
-            <div>
             <img alt={person.name} src={`https://image.tmdb.org/t/p/w200${person.profile_path}`} />
             <Link to={toPerson(person)}>{person.name}</Link>
-        </div>
+            {showJob || <p> {person.job} </p>}
+            {showCharacter || <p> {person.character} </p>}
         </StyledPersonTile>
     );
 }
