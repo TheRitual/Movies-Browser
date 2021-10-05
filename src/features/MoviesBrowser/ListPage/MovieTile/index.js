3f4b110372c93f059015257c1c7495b0c01340fe
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-05 13:19:53 +0200

    StyledLink imported

--- src/features/MoviesBrowser/ListPage/MovieTile/index.js
+++ src/features/MoviesBrowser/ListPage/MovieTile/index.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 import Vote from "../../../../common/Vote";
 import { toMovie } from "../../../../core/config/routes";
-import { StyledMovieTile } from "./styled";
+import { 
+    StyledMovieTile,
+    StyledLink,
+
+ } from "./styled";
 
 const MovieTile = ({ movie }) => {
     return (
         <StyledMovieTile>
-            <div>
+
             <img alt={movie.title} src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} />
-            <p><Link to={toMovie(movie)}>{movie.title}</Link></p>
+
+            <StyledLink>
+            <Link to={toMovie(movie)}>{movie.title}</Link>
+            </StyledLink>
+            
             <p> Release data: {movie.release_date} </p>
             <p> Genres:&nbsp;
                 {movie.genre_ids && movie.genre_ids.map(genre => (
@@ -18,7 +26,7 @@ const MovieTile = ({ movie }) => {
                 ))}
             </p>
             <p><Vote score={movie.vote_average} count={movie.vote_count} /></p>
-        </div>
+       
         </StyledMovieTile>
     );
 }
