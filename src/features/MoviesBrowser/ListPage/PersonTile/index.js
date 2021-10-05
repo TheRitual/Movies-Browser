5592af2964db6744cb1f278ba2d2648d47962ca5
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-05 14:02:17 +0200

    imported styled poster and link

--- src/features/MoviesBrowser/ListPage/PersonTile/index.js
+++ src/features/MoviesBrowser/ListPage/PersonTile/index.js
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
+
 import { toPerson } from "../../../../core/config/routes";
-import { StyledPersonTile } from "./styled";
+import { StyledPersonTile, StyledLink, StyledPoster } from "./styled";
 
 const PersonTile = ({ person }) => {
+    
     return (
         <StyledPersonTile>
-            <div>
-            <img alt={person.name} src={`https://image.tmdb.org/t/p/w200${person.profile_path}`} />
-            <Link to={toPerson(person)}>{person.name}</Link>
-        </div>
+            <StyledPoster
+            people={person.name} 
+            src={`https://image.tmdb.org/t/p/w200${person.profile_path}`}
+            />
+            <StyledLink
+            to={toPerson(person)}>{person.name}
+            
+            </StyledLink>
         </StyledPersonTile>
     );
 }
