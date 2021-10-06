23e38945e804105c35ae70d207de2c9281f60f7e
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-10-06 20:00:50 +0200

    wrapper replaced

--- src/features/MoviesBrowser/PersonDetailPage/PersonDetailsTile/index.js
+++ src/features/MoviesBrowser/PersonDetailPage/PersonDetailsTile/index.js
@@ -13,37 +13,34 @@ import {
 const PersonDetailsTile = () => {
     const person = useSelector(selectDetailItem);
     return (
-
+<TileInnerWrapper>
         <PersonDetailTile>
             <TileInnerWrapper>
                 <StyledPoster
                     src={`https://image.tmdb.org/t/p/w400${person.profile_path}`}
                     alt={person.name} />
-
-                <PersonName
+{/* Content */}
+                <PersonName /*title*/
                     alt={person.name}
                 />
-
                 <Subtitle>
-
                     Birthday:
                     <StyledDetails>{person.birthday}</StyledDetails>
                 </Subtitle>
 
                 <Subtitle>
-
                     Place of birth:
                     <StyledDetails>{person.place_of_birth}</StyledDetails>
                 </Subtitle>
 
-
                 <Description>
                     <p>Bio: {person.biography}</p>
                 </Description>
-
+{/* Content */}
 
             </TileInnerWrapper>
         </PersonDetailTile>
+        </TileInnerWrapper>
 
 
     );
