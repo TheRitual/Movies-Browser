72f9e616c4927b4f62a32b0d0e5aeaacff9908bd
Ritual <dev@theritual.eu>
2021-10-12 13:15:44 +0200

    updated dummy Images

--- src/features/MoviesBrowser/ListPage/PersonTile/index.js
+++ src/features/MoviesBrowser/ListPage/PersonTile/index.js
@@ -1,8 +1,11 @@
 
 import { toPerson } from "../../../../core/config/routes";
 import { StyledPersonTile, StyledLink, StyledPoster, PersonDetails } from "./styled";
+import PersonDummy from "../../../../assets/images/person_dummy.svg";
+
 
 const PersonTile = ({ person, showJob, showCharacter }) => {
+    const imgSrc = person.profile_path ? `https://image.tmdb.org/t/p/w400${person.profile_path}` : PersonDummy;
 
     return (
         <StyledPersonTile>
@@ -10,7 +13,7 @@ const PersonTile = ({ person, showJob, showCharacter }) => {
             to={toPerson(person)}>
             <StyledPoster
                 alt={person.name}
-                src={`https://image.tmdb.org/t/p/w200${person.profile_path}`}
+                src={imgSrc}
                 />
                 {person.name}
             </StyledLink>
