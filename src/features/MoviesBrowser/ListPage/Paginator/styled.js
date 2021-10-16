259fdd24f79ef282a8c4ea76acf3e972d738c439
Ritual <dev@theritual.eu>
2021-10-16 22:13:54 +0200

    mobile version of paginator

--- src/features/MoviesBrowser/ListPage/Paginator/styled.js
+++ src/features/MoviesBrowser/ListPage/Paginator/styled.js
@@ -5,18 +5,41 @@ import { ReactComponent as rightArrowSVG } from "../../../../assets/images/right
 export const LeftArrow = styled(leftArrowSVG)`
     width: 7px;
     height: 11px;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        height: 8px;
+        width: 5px;
+    }
 `;
 
 export const RightArrow = styled(rightArrowSVG)`
     width: 7px;
     height: 11px;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        height: 8px;
+        width: 5px;
+    }
+`;
+
+export const AdditionalLeftArrow = styled(LeftArrow)`
+    display: none;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        display: inline-block;
+    }
+`;
+
+export const AdditionalRightArrow = styled(RightArrow)`
+    display: none;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        display: inline-block;
+    }
 `;
 
 export const PaginatorButton = styled.button`
+    display: flex;
+    flex-wrap: nowrap;
     height: 36px;
     font-size: 14px;
     padding: 8px 16px;
-    margin: 12px;
     color: ${({ theme }) => theme.colors.paginatorButtonText};
     cursor: pointer;
     background: ${({ theme }) => theme.colors.paginatorButton};
@@ -35,14 +58,24 @@ export const PaginatorButton = styled.button`
             color: ${({ theme }) => theme.colors.paginatorDisabledArrow};
         }
     }
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        height: 23px;
+        padding: 8px 12px;
+        font-size: 8px;
+        gap: 4px;
+    }
 `;
 
 export const PaginatorText = styled.span`
     color: ${({ theme }) => theme.colors.paginatorText};
     font-size: 16px;
     line-height: 1.5;
-    margin-left: 24px;
-    margin-right: 24px;
+    margin: 0 12px;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        margin: 0;
+        font-size: 10px;
+        line-height: 2.4;
+    }
 `;
 
 export const PaginatorPage = styled.span`
@@ -51,7 +84,21 @@ export const PaginatorPage = styled.span`
 `;
 
 export const PaginatorWrapper = styled.div`
-    margin: 40px  auto 103px;
-    padding: 0px;
-    width: fit-content;
+    margin: 40px auto;
+    padding: 0;
+    width: 100%;
+    display: flex;
+    gap: 12px;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        gap: 8px;
+    }
+`;
+
+export const ButtonText = styled.span`
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        display: none;
+    }
 `;
