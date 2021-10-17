bc753189c0a1a3cb48f67cd24c2a994b735c5f1a
Ritual <dev@theritual.eu>
2021-10-17 02:56:19 +0200

    PersonalDetailTile mobile adapted

--- src/features/MoviesBrowser/PersonDetailPage/PersonDetailsTile/index.js
+++ src/features/MoviesBrowser/PersonDetailPage/PersonDetailsTile/index.js
@@ -8,8 +8,8 @@ import {
     StyledDetails,
     Description,
     StyledPoster,
-    DataWrapper,
     DetailsWrapper,
+    ImageWrapper,
 } from "./styled";
 
 const PersonDetailsTile = () => {
@@ -17,28 +17,30 @@ const PersonDetailsTile = () => {
     const imageSrc = person.profile_path ? `https://image.tmdb.org/t/p/w400${person.profile_path}` : DummyPerson;
     return (
         <PersonDetailTile>
-            <StyledPoster src={imageSrc} alt={person.name} />
-            <DataWrapper>
+            <ImageWrapper>
+                <StyledPoster src={imageSrc} alt={person.name} />
+            </ImageWrapper>
+            <DetailsWrapper>
                 <PersonName> {person.name} </PersonName>
-                <DetailsWrapper>
-                    {person.birthday &&
-                        <Subtitle>
-                            Date of birth:&nbsp;
-                            <StyledDetails>
-                                {person.birthday.toString().substring(8, 10)}.{person.birthday.toString().substring(5, 7)}.{person.birthday.toString().substring(0, 4)}
-                            </StyledDetails>
-                        </Subtitle>
-                    }
-                    {person.place_of_birth &&
-                        <Subtitle>
-                            Place of birth:&nbsp;<StyledDetails> {person.place_of_birth} </StyledDetails>
-                        </Subtitle>
-                    }
-                </DetailsWrapper>
-                {person.biography &&
-                    <Description> {person.biography} </Description>
+                {person.birthday &&
+                    <Subtitle>
+                        Birth:&nbsp;
+                        <StyledDetails>
+                            {person.birthday.toString().substring(8, 10)}.
+                            {person.birthday.toString().substring(5, 7)}.
+                            {person.birthday.toString().substring(0, 4)}
+                        </StyledDetails>
+                    </Subtitle>
                 }
-            </DataWrapper>
+                {person.place_of_birth &&
+                    <Subtitle>
+                        Place of birth:&nbsp;<StyledDetails> {person.place_of_birth} </StyledDetails>
+                    </Subtitle>
+                }
+            </DetailsWrapper>
+            {person.biography &&
+                <Description> {person.biography} </Description>
+            }
         </PersonDetailTile>
     );
 }
--- src/features/MoviesBrowser/PersonDetailPage/PersonDetailsTile/styled.js
+++ src/features/MoviesBrowser/PersonDetailPage/PersonDetailsTile/styled.js
@@ -3,25 +3,46 @@ import α from "color-alpha";
 
 export const PersonDetailTile = styled.div`
     display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: auto 1fr;
+    grid-column-gap: 40px;
+    grid-row-gap: 24px;
+    justify-items: stretch;
+    align-items: start;
+    grid-template-areas: "photo data" "photo description";
     width: 100%;
     background-color: ${({ theme }) => theme.colors.tileBackground};
     padding: 40px;
     box-shadow: 0px 4px 12px ${({ theme }) => α(theme.colors.tileShadow, 0.5)};
-    grid-template-columns: 1fr 2fr;
-    grid-gap: 40px;
     border-radius: 5px;
-    flex-direction: row;
     margin: 50px 0 64px;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        grid-template-rows: auto auto;
+        grid-template-areas: "photo data" "description description";
+        padding: 16px;
+        grid-column-gap: 16px;
+        grid-row-gap: 16px;
+    }
+`;
+
+export const ImageWrapper = styled.div`
+    grid-area: photo;
+    width: 100%;
 `;
 
 export const StyledPoster = styled.img`
     border-radius: 5px;
+    width: 100%;
 `;
 
-export const DataWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    row-gap: 24px;
+export const PersonName = styled.h1`
+    color: ${({ theme }) => theme.colors.linkHeader};
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1.3px;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 14px;
+    }
 `;
 
 export const DetailsWrapper = styled.div`
@@ -30,31 +51,35 @@ export const DetailsWrapper = styled.div`
     row-gap: 8px;
 `;
 
-export const PersonName = styled.h1`
-    color: ${({ theme }) => theme.colors.linkHeader};
-    font-weight: 600;
-    font-size: 36px;
-    line-height: 1.3px;
-`;
-
 export const Subtitle = styled.p`
     color: ${({ theme }) => theme.colors.subText};
     font-size: 18px;
     line-height: 1.2;
     margin: 0;
     padding: 0;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 12px;
+    }
 `;
 
 export const StyledDetails = styled.span`
+    grid-area: data;
     font-size: 18px;
     font-weight: normal;
     line-height: 1.2;
     color: ${({ theme }) => theme.colors.linkHeader};
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 12px;
+    }
 `;
 
 export const Description = styled.p`
+    grid-area: description;
     margin: 0;
     font-size: 20px;
     line-height: 1.6;
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 14px;
+    }
 `;
 
